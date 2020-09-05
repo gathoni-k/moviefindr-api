74b67bc556a160f1380681e0b40adc7c2b84d3da
gathoni-k <maryg.kahiga@gmail.com>
2020-09-06 02:29:14 +0300

    feat: Add movie search functionality

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const logger = require('morgan')
-
+import express from 'express'
+import logger from 'morgan'
+import movieRouter from './routes/movies.routes'
 const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
-
+app.use('/movies', movieRouter)
 module.exports = app
