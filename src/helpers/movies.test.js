1a407b18c4fc64f8293f7896f32d98b143f44b9b
gathoni-k <maryg.kahiga@gmail.com>
2020-09-06 02:20:28 +0300

    chore:  Add tests for movie search request

---
+++ src/helpers/movies.test.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import '../../startScripts/setupTests'
+import { fetchMovieSearch } from './movieData'
+import { movieSearchMockData } from './movies.search.mockData'
+
+jest.mock('axios')
+
+describe('fetch movie search data', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+  it('should fetch search data from an API', async (done) => {
+    try {
+      axios.get.mockResolvedValue(movieSearchMockData)
+      const response = await fetchMovieSearch('beauty', 1)
+      expect(response).toEqual(movieSearchMockData.data)
+      done()
+    } catch (error) {
+      done(error)
+    }
+  })
+  it('should return response of length 0', async (done) => {
+    const response = {
+      data: {
+        page: 190,
+        total_results: 1301,
+        total_pages: 66,
+        results: []
+      }
+    }
+    try {
+      axios.get.mockResolvedValue(response)
+      const data = await fetchMovieSearch('beauty', 190)
+      expect(data).toEqual(response.data)
+      done()
+    } catch (error) {
+      done(error)
+    }
+  })
+})
