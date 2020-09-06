9f3929dbfff09b6bd38e03233a5e02f78fe3869d
gathoni-k <maryg.kahiga@gmail.com>
2020-09-06 22:50:14 +0300

    chore: Add popular movies mock data results

---
+++ src/helpers/moviesMockData/movies.popular.js
@@ -0,0 +1,406 @@
+export const popularMovies = {
+  data: {
+    page: 1,
+    total_results: 10000,
+    total_pages: 500,
+    results: [
+      {
+        popularity: 1532.258,
+        vote_count: 148,
+        video: false,
+        poster_path: '/uOw5JD8IlD546feZ6oxbIjvN66P.jpg',
+        id: 718444,
+        adult: false,
+        backdrop_path: '/x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg',
+        original_language: 'en',
+        original_title: 'Rogue',
+        genre_ids: [
+          28
+        ],
+        title: 'Rogue',
+        vote_average: 6,
+        overview: 'Battle-hardened O’Hara leads a lively mercenary team of soldiers on a daring mission: rescue hostages from their captors in remote Africa. But as the mission goes awry and the team is stranded, O’Hara’s squad must face a bloody, brutal encounter with a gang of rebels.',
+        release_date: '2020-08-20'
+      },
+      {
+        popularity: 1089.551,
+        vote_count: 1065,
+        video: false,
+        poster_path: '/TnOeov4w0sTtV2gqICqIxVi74V.jpg',
+        id: 605116,
+        adult: false,
+        backdrop_path: '/qVygtf2vU15L2yKS4Ke44U4oMdD.jpg',
+        original_language: 'en',
+        original_title: 'Project Power',
+        genre_ids: [
+          28,
+          80,
+          878
+        ],
+        title: 'Project Power',
+        vote_average: 6.7,
+        overview: 'An ex-soldier, a teen and a cop collide in New Orleans as they hunt for the source behind a dangerous new pill that grants users temporary superpowers.',
+        release_date: '2020-08-14'
+      },
+      {
+        popularity: 1009.859,
+        vote_count: 15,
+        video: false,
+        poster_path: '/ugZW8ocsrfgI95pnQ7wrmKDxIe.jpg',
+        id: 724989,
+        adult: false,
+        backdrop_path: '/86L8wqGMDbwURPni2t7FQ0nDjsH.jpg',
+        original_language: 'en',
+        original_title: 'Hard Kill',
+        genre_ids: [
+          28,
+          53
+        ],
+        title: 'Hard Kill',
+        vote_average: 5.8,
+        overview: 'The work of billionaire tech CEO Donovan Chalmers is so valuable that he hires mercenaries to protect it, and a terrorist group kidnaps his daughter just to get it.',
+        release_date: '2020-08-25'
+      },
+      {
+        popularity: 983.842,
+        vote_count: 310,
+        video: false,
+        poster_path: '/sy6DvAu72kjoseZEjocnm2ZZ09i.jpg',
+        id: 581392,
+        adult: false,
+        backdrop_path: '/gEjNlhZhyHeto6Fy5wWy5Uk3A9D.jpg',
+        original_language: 'ko',
+        original_title: '반도',
+        genre_ids: [
+          28,
+          27,
+          53
+        ],
+        title: 'Peninsula',
+        vote_average: 7.2,
+        overview: 'A soldier and his team battle hordes of post-apocalyptic zombies in the wastelands of the Korean Peninsula',
+        release_date: '2020-07-15'
+      },
+      {
+        popularity: 950.62,
+        vote_count: 32,
+        video: false,
+        poster_path: '/n6hptKS7Y0ZjkYwbqKOK3jz9XAC.jpg',
+        id: 594328,
+        adult: false,
+        backdrop_path: '/lkeBhXGJFRlhI7cBWn8LQQAdZqK.jpg',
+        original_language: 'en',
+        original_title: 'Phineas and Ferb  The Movie Candace Against the Universe',
+        genre_ids: [
+          16,
+          35,
+          10402,
+          878,
+          10751,
+          10770
+        ],
+        title: 'Phineas and Ferb  The Movie Candace Against the Universe',
+        vote_average: 7,
+        overview: 'Phineas and Ferb travel across the galaxy to rescue their older sister Candace, who has been abducted by aliens and taken to a utopia in a far-off planet, free of her pesky little brothers.',
+        release_date: '2020-08-28'
+      },
+      {
+        popularity: 915.501,
+        vote_count: 61,
+        video: false,
+        poster_path: '/i4kPwXPlM1iy8Jf3S1uuLuwqQAV.jpg',
+        id: 721452,
+        adult: false,
+        backdrop_path: '/riDrpqQtZpXGeiJdlmfcwwPH7nN.jpg',
+        original_language: 'en',
+        original_title: 'One Night in Bangkok',
+        genre_ids: [
+          28,
+          53
+        ],
+        title: 'One Night in Bangkok',
+        vote_average: 7.1,
+        overview: "A hit man named Kai flies into Bangkok, gets a gun, and orders a cab. He offers a professional female driver big money to be his all-night driver. But when she realizes Kai is committing brutal murders at each stop, it's too late to walk away. Meanwhile, an offbeat police detective races to decode the string of slayings before more blood is spilled.",
+        release_date: '2020-08-25'
+      },
+      {
+        popularity: 830.494,
+        vote_count: 193,
+        video: false,
+        poster_path: '/uGhQ2ZGBpzCj6wC5jUrybsZuPTI.jpg',
+        id: 539885,
+        adult: false,
+        backdrop_path: '/ekkuqt9Q2ad1F7xq2ZONP0oq36P.jpg',
+        original_language: 'en',
+        original_title: 'Ava',
+        genre_ids: [
+          28,
+          80,
+          18,
+          53
+        ],
+        title: 'Ava',
+        vote_average: 6.3,
+        overview: 'A black ops assassin is forced to fight for her own survival after a job goes dangerously wrong.',
+        release_date: '2020-08-06'
+      },
+      {
+        popularity: 779.246,
+        vote_count: 51,
+        video: false,
+        poster_path: '/eDnHgozW8vfOaLHzfpHluf1GZCW.jpg',
+        id: 606234,
+        adult: false,
+        backdrop_path: '/u9YEh2xVAPVTKoaMNlB5tH6pXkm.jpg',
+        original_language: 'en',
+        original_title: 'Archive',
+        genre_ids: [
+          878
+        ],
+        title: 'Archive',
+        vote_average: 6.2,
+        overview: '2038: George Almore is working on a true human-equivalent AI, and his latest prototype is almost ready. This sensitive phase is also the riskiest as he has a goal that must be hidden at all costs—being reunited with his dead wife.',
+        release_date: '2020-08-13'
+      },
+      {
+        popularity: 700.235,
+        vote_count: 85,
+        video: false,
+        poster_path: '/tM4hht0LdY06UbuxGR4LjK6adCD.jpg',
+        id: 613504,
+        adult: false,
+        backdrop_path: '/dZJJDmiwp0W1NE74SY5WV00v0Ec.jpg',
+        original_language: 'en',
+        original_title: 'After We Collided',
+        genre_ids: [
+          18,
+          10749
+        ],
+        title: 'After We Collided',
+        vote_average: 7.6,
+        overview: 'Tessa finds herself struggling with her complicated relationship with Hardin; she faces a dilemma that could change their lives forever.',
+        release_date: '2020-09-02'
+      },
+      {
+        popularity: 624.366,
+        vote_count: 552,
+        video: false,
+        poster_path: '/jHo2M1OiH9Re33jYtUQdfzPeUkx.jpg',
+        id: 385103,
+        adult: false,
+        backdrop_path: '/fKtYXUhX5fxMxzQfyUcQW9Shik6.jpg',
+        original_language: 'en',
+        original_title: 'Scoob!',
+        genre_ids: [
+          12,
+          16,
+          35,
+          10751
+        ],
+        title: 'Scoob!',
+        vote_average: 7.4,
+        overview: 'In Scooby-Doo’s greatest adventure yet, see the never-before told story of how lifelong friends Scooby and Shaggy first met and how they joined forces with young detectives Fred, Velma, and Daphne to form the famous Mystery Inc. Now, with hundreds of cases solved, Scooby and the gang face their biggest, toughest mystery ever: an evil plot to unleash the ghost dog Cerberus upon the world. As they race to stop this global “dogpocalypse,” the gang discovers that Scooby has a secret legacy and an epic destiny greater than anyone ever imagined.',
+        release_date: '2020-07-08'
+      },
+      {
+        popularity: 620.163,
+        vote_count: 133,
+        video: false,
+        poster_path: '/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg',
+        id: 618354,
+        adult: false,
+        backdrop_path: '/bazlsLkNuhy3IuhviepqvlMm2hV.jpg',
+        original_language: 'en',
+        original_title: 'Superman: Man of Tomorrow',
+        genre_ids: [
+          28,
+          16,
+          878
+        ],
+        title: 'Superman: Man of Tomorrow',
+        vote_average: 7.3,
+        overview: 'It’s the dawn of a new age of heroes, and Metropolis has just met its first. But as Daily Planet intern Clark Kent – working alongside reporter Lois Lane – secretly wields his alien powers of flight, super-strength and x-ray vision in the battle for good, there’s even greater trouble on the horizon.',
+        release_date: '2020-08-23'
+      },
+      {
+        popularity: 597.726,
+        vote_count: 27,
+        video: false,
+        poster_path: '/9Rj8l6gElLpRL7Kj17iZhrT5Zuw.jpg',
+        id: 734309,
+        adult: false,
+        backdrop_path: '/7fvdg211A2L0mHddvzyArRuRalp.jpg',
+        original_language: 'en',
+        original_title: 'Santana',
+        genre_ids: [
+          28
+        ],
+        title: 'Santana',
+        vote_average: 6.3,
+        overview: 'Two brothers — one a narcotics agent and the other a general — finally discover the identity of the drug lord who murdered their parents decades ago. They may kill each other before capturing the bad guys.',
+        release_date: '2020-08-28'
+      },
+      {
+        popularity: 573.09,
+        vote_count: 97,
+        video: false,
+        poster_path: '/sMO1v5TUf8GOJHbJieDXsgWT2Ud.jpg',
+        id: 438396,
+        adult: false,
+        backdrop_path: '/qGZe9qTuydxyJYQ60XDtEckzLR8.jpg',
+        original_language: 'es',
+        original_title: 'Orígenes secretos',
+        genre_ids: [
+          18,
+          53
+        ],
+        title: 'Unknown Origins',
+        vote_average: 6.2,
+        overview: 'In Madrid, Spain, a mysterious serial killer ruthlessly murders his victims by recreating the first appearance of several comic book superheroes. Cosme, a veteran police inspector who is about to retire, works on the case along with the tormented inspector David Valentín and his own son Jorge Elías, a nerdy young man who owns a comic book store.',
+        release_date: '2020-08-28'
+      },
+      {
+        popularity: 571.75,
+        vote_count: 65,
+        video: false,
+        poster_path: '/5pe30v0z4ucVgwh5nR439cCzwwO.jpg',
+        id: 632618,
+        adult: false,
+        backdrop_path: '/cVdYaAQmd5DZNdo0KFJruz7JpUs.jpg',
+        original_language: 'es',
+        original_title: 'Crímenes de familia',
+        genre_ids: [
+          80,
+          18,
+          53
+        ],
+        title: 'The Crimes That Bind',
+        vote_average: 7,
+        overview: 'When her son is accused of raping and trying to murder his ex-wife, Alicia embarks on a journey that will change her life forever.',
+        release_date: '2020-08-20'
+      },
+      {
+        popularity: 559.863,
+        vote_count: 372,
+        video: false,
+        poster_path: '/kPzcvxBwt7kEISB9O4jJEuBn72t.jpg',
+        id: 677638,
+        adult: false,
+        backdrop_path: '/pO1SnM5a1fEsYrFaVZW78Wb0zRJ.jpg',
+        original_language: 'en',
+        original_title: 'We Bare Bears: The Movie',
+        genre_ids: [
+          12,
+          16,
+          35,
+          10751
+        ],
+        title: 'We Bare Bears: The Movie',
+        vote_average: 7.8,
+        overview: "When Grizz, Panda, and Ice Bear's love of food trucks and viral videos went out of hand, it catches the attention of Agent Trout from the National Wildlife Control, who pledges to restore the “natural order” by separating them forever. Chased away from their home, the Bears embark on an epic road trip as they seek refuge in Canada, with their journey being filled with new friends, perilous obstacles, and huge parties. The risky journey also forces the Bears to face how they first met and became brothers, in order to keep their family bond from splitting apart.",
+        release_date: '2020-06-30'
+      },
+      {
+        popularity: 518.197,
+        vote_count: 100,
+        video: false,
+        poster_path: '/4V2nTPfeB59TcqJcUfQ9ziTi7VN.jpg',
+        id: 501979,
+        adult: false,
+        backdrop_path: '/oazPqs1z78LcIOFslbKtJLGlueo.jpg',
+        original_language: 'en',
+        original_title: 'Bill & Ted Face the Music',
+        genre_ids: [
+          12,
+          35,
+          878
+        ],
+        title: 'Bill & Ted Face the Music',
+        vote_average: 6.5,
+        overview: 'Yet to fulfill their rock and roll destiny, the now middle-aged best friends Bill and Ted set out on a new adventure when a visitor from the future warns them that only their song can save life as we know it. Along the way, they are helped by their daughters, a new batch of historical figures and a few music legends—to seek the song that will set their world right and bring harmony to the universe.',
+        release_date: '2020-08-27'
+      },
+      {
+        popularity: 504.533,
+        vote_count: 94,
+        video: false,
+        poster_path: '/A11Ez4UkOE4Ysmtmur5Bho8qrGM.jpg',
+        id: 611395,
+        adult: false,
+        backdrop_path: '/qXACJOuyklS0BpvO8ALLkkrsv7W.jpg',
+        original_language: 'zh',
+        original_title: '征途',
+        genre_ids: [
+          28,
+          12,
+          14
+        ],
+        title: 'Double World',
+        vote_average: 6.9,
+        overview: 'Keen to bring honor to his clan, young villager Dong Yilong embarks on a perilous journey to compete in a tournament that selects warriors for battle.',
+        release_date: '2020-07-24'
+      },
+      {
+        popularity: 493.836,
+        vote_count: 131,
+        video: false,
+        poster_path: '/vFIHbiy55smzi50RmF8LQjmpGcx.jpg',
+        id: 703771,
+        adult: false,
+        backdrop_path: '/owraiceOKtSOa3t8sp3wA9K2Ox6.jpg',
+        original_language: 'en',
+        original_title: 'Deathstroke: Knights & Dragons - The Movie',
+        genre_ids: [
+          28,
+          16
+        ],
+        title: 'Deathstroke: Knights & Dragons - The Movie',
+        vote_average: 7,
+        overview: "Ten years ago, Slade Wilson-aka the super-assassin called Deathstroke-made a tragic mistake and his wife and son paid a terrible price. Now, a decade later, Wilson's family is threatened once again by the murderous Jackal and the terrorists of H.I.V.E. Can Deathstroke atone for the sins of the past-or will his family pay the ultimate price?",
+        release_date: '2020-08-04'
+      },
+      {
+        popularity: 480.885,
+        vote_count: 121,
+        video: false,
+        poster_path: '/3eg0kGC2Xh0vhydJHO37Sp4cmMt.jpg',
+        id: 531499,
+        adult: false,
+        backdrop_path: '/zogWnCSztU8xvabaepQnAwsOtOt.jpg',
+        original_language: 'en',
+        original_title: 'The Tax Collector',
+        genre_ids: [
+          28,
+          80,
+          18
+        ],
+        title: 'The Tax Collector',
+        vote_average: 6,
+        overview: 'David Cuevas is a family man who works as a gangland tax collector for high ranking Los Angeles gang members. He makes collections across the city with his partner Creeper making sure people pay up or will see retaliation. An old threat returns to Los Angeles that puts everything David loves in harm’s way.',
+        release_date: '2020-08-07'
+      },
+      {
+        popularity: 459.642,
+        vote_count: 789,
+        video: false,
+        poster_path: '/tI8ocADh22GtQFV28vGHaBZVb0U.jpg',
+        id: 475430,
+        adult: false,
+        backdrop_path: '/o0F8xAt8YuEm5mEZviX5pEFC12y.jpg',
+        original_language: 'en',
+        original_title: 'Artemis Fowl',
+        genre_ids: [
+          28,
+          12,
+          14,
+          878,
+          10751
+        ],
+        title: 'Artemis Fowl',
+        vote_average: 5.8,
+        overview: "Artemis Fowl is a 12-year-old genius and descendant of a long line of criminal masterminds. He soon finds himself in an epic battle against a race of powerful underground fairies who may be behind his father's disappearance.",
+        release_date: '2020-06-12'
+      }
+    ]
+  }
+}
