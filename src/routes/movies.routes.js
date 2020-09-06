ecb2d8f35230b9108fe484f5cfdc41c9ada9b344
gathoni-k <maryg.kahiga@gmail.com>
2020-09-06 04:09:14 +0300

    feat: Add movie details endpoint

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { searchMovie, trendingMovies } from '../controllers/movies.controllers'
+import { searchMovie, trendingMovies, movieDetails } from '../controllers/movies.controllers'
 
 const router = express.Router()
 
 router.get('/search', searchMovie)
 
 router.get('/trending/:time', trendingMovies)
+
+router.get('/:movieId', movieDetails)
 export default router
