6efee5239e2177abd3b03cdc0b9240f57dbf3dd8
gathoni-k <maryg.kahiga@gmail.com>
2020-09-07 13:43:17 +0300

    chore: Add controller for top rated movies route

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { searchMovie, trendingMovies, movieDetails, popularMovies } from '../controllers/movies.controllers'
+import { searchMovie, trendingMovies, movieDetails, popularMovies, topMovies } from '../controllers/movies.controllers'
 
 const router = express.Router()
+
+router.get('/top-rated', topMovies)
 
 router.get('/popular', popularMovies)
 
@@ -11,5 +13,4 @@ router.get('/trending/:time', trendingMovies)
 
 router.get('/:movieId', movieDetails)
 
-router.get('/top-rated')
 export default router
